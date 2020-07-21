e5794f6a9007f09962e298beaf1e6e00b79178ea
Abinash Panda <abinash@prodios.com>
2020-07-21 13:59:25 +0530

    Add focus styles

--- client/src/ui-kit/Input/Input.tsx
+++ client/src/ui-kit/Input/Input.tsx
@@ -37,7 +37,7 @@ const Input = forwardRef(
         />
         {type === 'password' ? (
           <button
-            className="text-gray-500 focus:outline-none"
+            className="text-gray-500 focus:outline-none focus:text-green-500"
             onClick={togglePasswordVisibility}
             type="button"
           >
