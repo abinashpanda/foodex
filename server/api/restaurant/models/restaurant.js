3679210b5800a0086924e59f3f27d54445605b21
Abinash Panda <abinash@prodios.com>
2020-07-25 16:45:25 +0530

    Add slug in restaurant

--- server/api/restaurant/models/restaurant.js
+++ server/api/restaurant/models/restaurant.js
@@ -1,8 +1,21 @@
 'use strict';
 
+const slugify = require('slugify');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      data.slug = slugify(data.name);
+    },
+  },
+};
